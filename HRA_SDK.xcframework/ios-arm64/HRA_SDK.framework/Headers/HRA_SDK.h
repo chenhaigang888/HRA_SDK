a35b90b6d031fd62480f113d47f55688295b7030
hogan <329206444@qq.com>
2023-06-02 10:07:28 +0800

    save

---
+++ HRA_SDK.xcframework/ios-arm64/HRA_SDK.framework/Headers/HRA_SDK.h
@@ -0,0 +1,18 @@
+//
+//  HRA_SDK.h
+//  HRA_SDK
+//
+//  Created by hogan on 2023/6/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HRA_SDK.
+FOUNDATION_EXPORT double HRA_SDKVersionNumber;
+
+//! Project version string for HRA_SDK.
+FOUNDATION_EXPORT const unsigned char HRA_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRA_SDK/PublicHeader.h>
+
+
